b6f4a1d4cb7f025301227f3fbda42adb1f152813
elmiguel0 <miguelangel.ramos@ibm.com>
2022-12-08 16:40:45 -0600

    changes done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 1; i <= sideLength ; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                int r = sideLength/2;
+                if(j==r+1)
+                {
+                    System.out.print(8);
+                }
+                else if(i==r+1)
+                {
+                    System.out.print(8);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,37 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+        for (int i = 0; i < height/2; i++) {
+            for (int j = 0; j < height; j++) {
+                if(i==0){
+                    System.out.print(8);
+                }else if(i<=j && j<height-i) {
+                    System.out.print(8);
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        if(height%2!=0)
+        {
+            for (int i = 0; i < height; i++) {
+                if(i==height/2)
+                    System.out.print(8);
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = height/2; i > 0; i--) {
+            for (int j = 0; j < height; j++) {
+                if(i<=j+1 && j<=height-i) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
